275ae26279945cd0cf40ae2b55ab64cec5a9affe
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2024-06-15 00:44:13 +0530

    integrated with cheerio

--- index.js
+++ index.js
@@ -2,111 +2,95 @@ const cors = require("cors");
 const express = require("express");
 const axios = require("axios");
 const dotenv = require("dotenv");
+const cheerio = require("cheerio");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 dotenv.config();
-let visitedURL = "";
-let html = "";
-let isHtmlContainsMore = false; // for arrays
-let tempHtml = "";
+let lastVisitedURL = "";
+let $ = null; // parsed html
 
-const getInnerHTML = async (url, classString) => {
-  let result;
+const getValue = async (url, classString, dataType, isSingle) => {
+  let value;
   try {
-    if (visitedURL !== url) {
+    if (lastVisitedURL !== url) {
       const res = await axios.get(url);
-      html = res.data;
-      visitedURL = url;
+      const html = res.data;
+      $ = cheerio.load(html);
+      lastVisitedURL = url;
     }
-    if (!html.includes('"' + classString + '"')) {
-      return false;
-    }
-    const classIndex = html.indexOf('"' + classString + '"');
-    let startIndex = classIndex + 1;
-    while (html[startIndex++] != ">");
-    let endIndex = startIndex + 1;
-    while (html[endIndex++] != "<");
-    result = html.slice(startIndex, endIndex - 1).trim(" ");
-    if (html.slice(endIndex).includes('"' + classString + '"')) {
-      if (!tempHtml) {
-        tempHtml = html;
+    if (!classString || !url) {
+      // default value for empty class strings
+      if (isSingle) {
+        return null;
       }
-      isHtmlContainsMore = true;
-      html = html.slice(endIndex);
-    } else {
-      isHtmlContainsMore = false;
-      if (tempHtml) {
-        html = tempHtml;
-        tempHtml = "";
-      }
+      return [null];
     }
+    const classesArr = classString.split(" ").filter((cls) => cls !== "");
+    const elements = $(`.${classesArr.join(".")}`); // could be one or many elements (with same classes set)
+    elements.each((index, element) => {
+      const directText = $(element)
+        .contents()
+        .filter((index, node) => node.type === "text")
+        .text();
+      let tempValue;
+      switch (dataType) {
+        case "number":
+          tempValue = parseInt(directText.replace(/[^0-9]/g, ""));
+          break;
+        case "boolean":
+          tempValue = Boolean(directText);
+          break;
+        default:
+          tempValue = directText.trim("");
+      }
+      if (isSingle) {
+        value = tempValue;
+        return false; // break out of the loop
+      }
+      if (Array.isArray(value)) {
+        value.push(tempValue);
+      } else {
+        value = [tempValue];
+      }
+    });
   } catch (e) {
     // console.log("error");
-    return null;
+    if (isSingle) {
+      return null;
+    }
+    return [null];
   }
-  if (result) {
-    return result;
+  if (value) {
+    return value;
   }
 };
 const getJSON = async (urls, classes, keys, types, searchTypes) => {
   const arr = [];
   for (let i = 0; i < urls.length; i++) {
     const url = urls[i];
-    if (url === "") {
-      continue;
-    }
     const obj = {};
     for (let j = 0; j < classes.length; j++) {
-      if (classes[j] === "") {
-        continue;
-      }
       if (keys[j] === "") {
         keys[j] = "key-" + Number(j + 1);
       }
-      const innerHtml = await getInnerHTML(url, classes[j]);
-      if (innerHtml === false) {
-        continue;
-      } else if (innerHtml === null || innerHtml[0] === "<") {
-        j -= 1;
-      } else {
-        let value;
-        switch (types[j]) {
-          case "number":
-            value = parseInt(innerHtml.replace(/[^0-9]/g, ""));
-            break;
-          case "boolean":
-            value = Boolean(innerHtml);
-            break;
-          default:
-            value = innerHtml;
-        }
-        if (searchTypes[j] == "single") {
-          obj[keys[j]] = value;
-        } else if (!isHtmlContainsMore) {
-          if (!(keys[j] in obj)) {
-            obj[keys[j]] = value;
-          } else {
-            obj[keys[j]].push(value);
-          }
-        } else {
-          if (!(keys[j] in obj)) {
-            obj[keys[j]] = [value];
-          } else {
-            obj[keys[j]].push(value);
-          }
-          j -= 1;
-        }
-      }
+      const value = await getValue(
+        url,
+        classes[j],
+        types[j],
+        searchTypes[j] === "single"
+      );
+      obj[keys[j]] = value;
     }
     arr.push(obj);
   }
-  visitedURL = "";
-  html = "";
+  lastVisitedURL = "";
+  $ = null;
   return arr;
 };
+
 app.post("/getJSON", async (req, res) => {
   const urls = req.body.urls;
   const classes = req.body.classes;
@@ -116,6 +100,7 @@ app.post("/getJSON", async (req, res) => {
   const arr = await getJSON(urls, classes, keys, types, searchTypes);
   res.json(arr);
 });
+
 app.get("/", (req, res) => {
   console.log("got a request");
   res.json({
